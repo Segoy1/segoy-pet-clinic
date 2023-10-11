07c5b6f8899d071d159b1f785db7329d33812927
Segoy <segoy.thiel@protonmail.com>
2023-10-11 21:00:47 +0200

    added services and still some files from release.

---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/OwnerService.java
@@ -0,0 +1,15 @@
+package de.segoy.springframework.services;
+
+import de.segoy.springframework.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/VetService.java
@@ -0,0 +1,14 @@
+package de.segoy.springframework.services;
+
+import de.segoy.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
