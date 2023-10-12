c8c0871066e11f52ccf28f95efb4d7ad11425e40
Segoy <segoy.thiel@protonmail.com>
2023-10-12 09:35:25 +0200

    reafctoring to common crud interface. closes #5

--- pet-clinic-data/src/main/java/de/segoy/springframework/services/VetService.java
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/VetService.java
@@ -2,13 +2,7 @@ package de.segoy.springframework.services;
 
 import de.segoy.springframework.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
