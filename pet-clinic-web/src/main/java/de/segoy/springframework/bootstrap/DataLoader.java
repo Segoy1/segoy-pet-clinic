c1578da1f9650c424ef34f3ff59fe8f4704bbf26
Segoy <segoy.thiel@protonmail.com>
2023-10-13 08:48:29 +0200

    added set id Method to Map Service

--- pet-clinic-web/src/main/java/de/segoy/springframework/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/de/segoy/springframework/bootstrap/DataLoader.java
@@ -24,7 +24,6 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Klaus");
         owner1.setLastName("Lang");
 
@@ -32,7 +31,6 @@ public class DataLoader implements CommandLineRunner {
 
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Hans");
         owner2.setLastName("Lang");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Peter");
         vet1.setLastName("Kurz");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Fritz");
         vet2.setLastName("Kurz");
 
