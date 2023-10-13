d4afb9755741e38b8dc2e9e8fed7f9d838a9f144
Segoy <segoy.thiel@protonmail.com>
2023-10-13 13:46:11 +0200

    added Specialty Service. closes #24. Last commit: closes #26

---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package de.segoy.springframework.services;
+
+import de.segoy.springframework.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty,Long> {
+}
---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package de.segoy.springframework.services.map;
+
+import de.segoy.springframework.model.Specialty;
+import de.segoy.springframework.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findall();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
