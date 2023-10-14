5f2c9589e9733ae4205db8345580a8846dec1953
Segoy <segoy.thiel@protonmail.com>
2023-10-14 19:37:19 +0200

    Specialty JPA Service. closes #37

---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/springdatajpa/SpecialtySDJPAService.java
@@ -0,0 +1,48 @@
+package de.segoy.springframework.services.springdatajpa;
+
+import de.segoy.springframework.model.Specialty;
+import de.segoy.springframework.repositories.SpecialtyRepository;
+import de.segoy.springframework.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Profile("springdatajpa")
+@Service
+public class SpecialtySDJPAService implements SpecialtyService {
+
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJPAService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return  specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialtyRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialtyRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialtyRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialtyRepository.deleteById(aLong);
+    }
+}
