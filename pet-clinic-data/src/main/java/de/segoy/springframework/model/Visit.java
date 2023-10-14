e27c9355e01a52dd77f259277e5cd62d1d4a69f7
Segoy <segoy.thiel@protonmail.com>
2023-10-14 18:15:19 +0200

    added Visit Entity. closes #31

--- pet-clinic-data/src/main/java/de/segoy/springframework/model/Visit.java
+++ pet-clinic-data/src/main/java/de/segoy/springframework/model/Visit.java
@@ -1,10 +1,20 @@
 package de.segoy.springframework.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+
+    @Column
     private LocalDate date;
+
+    @Column
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
