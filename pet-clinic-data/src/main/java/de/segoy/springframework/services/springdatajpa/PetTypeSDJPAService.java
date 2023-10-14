bf311a985645473fd142ba1b6296dfc5ebf92f8b
Segoy <segoy.thiel@protonmail.com>
2023-10-14 19:30:44 +0200

    added Pet Type JPA Service. closes #35

---
+++ pet-clinic-data/src/main/java/de/segoy/springframework/services/springdatajpa/PetTypeSDJPAService.java
@@ -0,0 +1,48 @@
+package de.segoy.springframework.services.springdatajpa;
+
+import de.segoy.springframework.model.PetType;
+import de.segoy.springframework.repositories.PetTypeRepository;
+import de.segoy.springframework.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Profile("springdatajpa")
+@Service
+public class PetTypeSDJPAService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJPAService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
