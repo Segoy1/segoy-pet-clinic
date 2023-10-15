529aa9570713491105f1ede4afbf5ce8637be5d6
Segoy <segoy.thiel@protonmail.com>
2023-10-15 11:43:10 +0200

    added Owner Map Service Test. closes #42

--- pet-clinic-web/src/test/java/de/segoy/springframework/SegoyPetClinicApplicationTest.java
+++ pet-clinic-web/src/test/java/de/segoy/springframework/SegoyPetClinicApplicationTest.java
@@ -1,10 +1,16 @@
 package de.segoy.springframework;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SegoyPetClinicApplicationTest {
+
+    @Test
+    void testSpringPetClinicApplication(){
+
+    }
 
 }
